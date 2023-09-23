000dd8838b060a081de0aa0c18eb73ec683e620b
beegdady <adelekemanuel@gmail.com>
2023-09-23 05:50:49 -0700

    Adding Javascript to ensure number functionality

--- class2/AndroidCalculator/scripts.js
+++ class2/AndroidCalculator/scripts.js
@@ -1,5 +1,63 @@
 
 
+const dText = document.getElementById('dText');
+const dRes = document.getElementById('dRes');
+var buttons = document.getElementsByClassName('btn');
+let currentValue = "";
+
+
+
+for (i = 0; i < buttons.length; i++){
+    const button = buttons[i];
+
+
+    button.addEventListener('click', function() {
+        let buttonValue = button.value;
+        let dispVal = dText.value;
+
+        if (buttonValue == 'C'){
+            currentValue = "";
+            // dText.value = currentValue;
+
+        }else if(buttonValue == '%'){
+            let number = Number(dispVal);
+
+            if (isNumeric(number)) {
+                let result = number/100;
+                dRes.value = result;
+            }
+        }else if(buttonValue =='()'){
+            if (!dispVal.includes('(')){
+                currentValue += '(';
+            }else{
+                let lastChar = dispVal.slice(-1);
+                if (!isNaN(lastChar) || lastChar === ')'){
+                    currentValue += ')'
+                }else{
+                    currentValue += '('
+                }
+            }
+        }
+        
+        else{
+            currentValue += buttonValue;
+            console.log(currentValue);
+        }
+
+        dText.value = currentValue;
+        
+    });
+}
+
+
+function isNumeric(input) {
+    return /^\d+$/.test(input);
+}
+
+
+
+
+
 const togSci = document.querySelector('.sci-more');
 const togSci1 = document.querySelector('.sci-more1');
 
